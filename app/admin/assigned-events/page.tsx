19f7590e37bc972602b750e4a7f668d1800431d3
sachinchauhan <010sssachin@gmail.com>
2026-05-21 15:30:37 +0530

    download qr for registration 2

--- app/admin/assigned-events/page.tsx
+++ app/admin/assigned-events/page.tsx
@@ -42,7 +42,7 @@ export default function AssignEventsPage() {
 		setDownloadingQR(eventId);
 		try {
 			const response = await apiClient.get(
-				`/api/admin/manage-event/${eventId}/registration-qr`,
+				`/api/admin/manage-event/${eventId}/registration-qrcode`,
 				{ responseType: 'blob' }
 			);
 
